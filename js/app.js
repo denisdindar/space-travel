e2f68870653a850ecc3a3fa0131b67954a02a50d
Denis Din <dnzwebio@gmail.com>
2021-12-13 01:22:41 +0700

    final

--- js/app.js
+++ js/app.js
@@ -1,115 +1,228 @@
-// select elements
-const nav = document.querySelector("#mainNav");
-const items = nav.querySelectorAll(".nav__item");
-const item = ".nav__item";
-const tab = document.querySelector("#tabs");
-const tabItems = document.querySelectorAll(".tabs__item");
+// SHORTHANDS
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+// SELECTING ELEMENTS
+// - change active states on click
+// -- main navigation
+const navArea = $("#nav");
+const navItems = $$(".nav__item");
+const navItem = ".nav__item";
+// -- destination tabs
+const tabArea = $("#tabs");
+const tabItems = $$(".tabs__item");
 const tabItem = ".tabs__item";
-const sections = document.querySelectorAll(".section");
-const background = document.querySelector(".bg");
-const buttonMain = document.querySelector(".btn--main");
-const secDestination = document.querySelector(".destination");
-const secDestinationData = document.querySelector(
-  "[data-section='destination']"
-);
-const nmbr = document.querySelector("#numbers");
-const nmbrItems = document.querySelectorAll(".btn--numbers");
-const nmbrItem = ".btn--numbers";
-
-const dot = document.querySelector("#dots");
-const dotItems = document.querySelectorAll(".btn--dots");
+// -- crew dots
+const dotArea = $("#dots");
+const dotItems = $$(".btn--dots");
 const dotItem = ".btn--dots";
+// -- technology numbers
+const numberArea = $("#numbers");
+const numberItems = $$(".btn--numbers");
+const numberItem = ".btn--numbers";
+// -- section and background
+const sections = $$(".section");
+const background = $(".bg");
+// -- home button to destination tab
+const btn = ".btn";
+const btnArea = $("#btn");
+const sectionDestination = $(".destination");
+const sectionDestinationData = $("[data-section='destination']");
 
-// FUNCTION: change active
-const changeActive = (ul, li, lis) => {
-  ul.addEventListener("click", function (e) {
-    // select to clicked li
-    const clicked = e.target.closest(li);
+// FUNCTIONS
+// - Change active state in lists/tabs on clicked
+const changeActive = (area, item, items) => {
+  area.addEventListener("click", function (e) {
+    // select the clicked child
+    const clicked = e.target.closest(item);
     console.log(clicked);
-    // do nothing if clicked area is outside of li
+    // do nothing if clicked area is outside/false
     if (!clicked) return;
     // remove active classes
-    lis.forEach((all) => {
+    items.forEach((all) => {
       all.classList.remove("active");
     });
-    // add active class to clicked li
+    // add active class to clicked child
     clicked.classList.add("active");
   });
 };
 
-// FUNCTION: change section and background
-const changeSection = () => {
-  nav.addEventListener("click", function (e) {
-    // select to clicked li
-    const selected = e.target.closest(".nav__item");
-    // console.log(selected);
-    // do nothing if selected area is outside of li
-    if (!selected) return;
-    // remove all visible sections and show selected section
-    // console.log(selected.dataset.section);
+// - Change sections and backgrounds on clicked
+const navigate = (y, area) => {
+  area.addEventListener("click", function (e) {
+    // select the clicked child
+    const clicked = e.target.closest(y);
+    if (!clicked) return;
+    // remove visibility of sections and add visible to clicked section
     sections.forEach((s) => s.classList.remove("visible"));
-    document
-      .querySelector(`.${selected.dataset.section}`)
-      .classList.add("visible");
-    // change background
-    background.classList.remove(
-      "bg--destination",
-      "bg--crew",
-      "bg--technology"
-    );
-    background.classList.add(`bg--${selected.dataset.section}`);
-  });
-};
-
-// FUNCTION: cta buttonMain onClick
-const changeCTA = () => {
-  buttonMain.addEventListener("click", function (s) {
-    // remove and add .visible class to main section
-    sections.forEach((s) => s.classList.remove("visible"));
-    secDestination.classList.add("visible");
-
-    // remove all active states in navigation and add .active class to destination link
-    items.forEach((all) => {
+    $(`.${clicked.dataset.section}`).classList.add("visible");
+    // remove active classes from navigation
+    navItems.forEach((all) => {
       all.classList.remove("active");
     });
-    secDestinationData.classList.add("active");
-
-    // change background
-    // change background
+    // add active class to destination tab (if clicked by main button)
+    $("#destination").classList.add("active");
+    // change related background on click
     background.classList.remove(
       "bg--destination",
       "bg--crew",
       "bg--technology"
     );
-    background.classList.add("bg--destination");
+    background.classList.add(`bg--${clicked.dataset.section}`);
   });
 };
 
-// Change sections with backgrounds onClick
-changeSection();
-// Change Section with background on CTA click
-changeCTA();
-// Change active classes onClick
-changeActive(nav, item, items);
-changeActive(tab, tabItem, tabItems);
-changeActive(dot, dotItem, dotItems);
-changeActive(nmbr, nmbrItem, nmbrItems);
+// - Do navigation
+navigate(navItem, navArea);
+navigate(btn, btnArea);
+
+// - Change active classes onClick
+changeActive(navArea, navItem, navItems);
+changeActive(tabArea, tabItem, tabItems);
+changeActive(dotArea, dotItem, dotItems);
+changeActive(numberArea, numberItem, numberItems);
 
 // FETCHING DATA
-// ------------------------------
-fetch("data.json")
+// - Reading data from data.json
+fetch("./data.json")
   .then((response) => response.json())
-  .then((responseJson) => {
-    console.log(responseJson);
+  // - Show data
+  .then((data) => {
+    console.log(data);
+
+    // -- DESTINATION SECTION
+    // destination section get data on first load
+    function loadDestination(xy) {
+      $("[data-animated-content-tabs]").innerHTML = `
+    <h2 class="tabs__title anim__focus-in-expand">${data.destinations[xy].name}</h2>
+    <p class="tabs__text anim__slide-top">${data.destinations[xy].description}</p>
+    <div class="tabs__details anim__tracking-in-expand">
+      <div class="tabs__details--distance">
+        <span>Avg. Distance</span> ${data.destinations[xy].distance}
+      </div>
+      <div class="tabs__details--time">
+        <span>Est. Travel Time</span> ${data.destinations[xy].travel}
+      </div>
+    </div>
+    `;
+      $(".destination__img").innerHTML = `
+      <img class="anim__planet" src="${data.destinations[xy].images.png}" alt="${data.destinations[xy].name}" />
+      `;
+    }
+    // call first array on first load (planet moon)
+    loadDestination(0);
+
+    // --- selecting tabs
+    const tabMoon = $('[data-tab="moon"]'),
+      tabMars = $('[data-tab="mars"]'),
+      tabEuropa = $('[data-tab="europa"]'),
+      tabTitan = $('[data-tab="titan"]');
+
+    // --- function to show data on click
+    const onClickDestination = (planet, array) => {
+      planet.addEventListener("click", () => {
+        // call code block in loadDestination function with array param
+        return loadDestination(array);
+      });
+    };
+
+    // --- create content of all tabs
+    // --- numbers below are coming from destinationDefault(array)
+    onClickDestination(tabMoon, 0);
+    onClickDestination(tabMars, 1);
+    onClickDestination(tabEuropa, 2);
+    onClickDestination(tabTitan, 3);
+
+    // -- CREW SECTION
+    // crew section get data on first load
+    function loadCrew(xy) {
+      $("[data-animated-content-slides]").innerHTML = `
+      <h5 class="crew__job anim__fade-in-left">${data.crew[xy].role}</h5>
+      <h3 class="crew__name anim__focus-in-expand">${data.crew[xy].name}</h3>
+      <p class="crew__bio anim__fade-in anim__delay--1">
+      ${data.crew[xy].bio}
+      </p>`;
+      $(".crew__img").innerHTML = `
+      <img class="anim__swing-in-bottom-fwd" src="${data.crew[xy].images.png}" alt="${data.crew[xy].name}" />
+      `;
+    }
+    // call first array on first load (planet moon)
+    loadCrew(0);
+
+    // --- selecting slides
+    const slide1 = $('[data-slide="slide-1"]'),
+      slide2 = $('[data-slide="slide-2"]'),
+      slide3 = $('[data-slide="slide-3"]'),
+      slide4 = $('[data-slide="slide-4"]');
+
+    // --- function to show data on click
+    const onclickCrew = (planet, array) => {
+      planet.addEventListener("click", () => {
+        // call code block in loadCrew function with array param
+        return loadCrew(array);
+      });
+    };
+
+    // --- create content of all slides
+    // --- numbers below are coming from destinationDefault(array)
+    onclickCrew(slide1, 0);
+    onclickCrew(slide2, 1);
+    onclickCrew(slide3, 2);
+    onclickCrew(slide4, 3);
+
+    // -- TECH SECTION
+    // crew section get data on first load
+    function loadTech(xy) {
+      $("[data-animated-content-numbers]").innerHTML = `
+      <h5 class="technology__article--sub anim__fade-in-left">Terminology ...</h5>
+      <h3 class="technology__article--title anim__focus-in-expand">${data.technology[xy].name}</h3>
+      <p class="technology__article--text anim__slide-top">${data.technology[xy].description}</p>
+      `;
+      $(".technology__img").innerHTML = `
+      <picture>
+      <source
+        media="(max-width:1024px)"
+        srcset="${data.technology[xy].images.landscape}"
+      />
+      <img class="anim__scale-up-hor-right" src="${data.technology[xy].images.portrait}" alt="${data.technology[xy].name}" />
+      </picture>
+      `;
+    }
+    // call first array on first load (planet moon)
+    loadTech(0);
+
+    // --- selecting bumbers
+    const tech1 = $('[data-no="tech-1"]'),
+      tech2 = $('[data-no="tech-2"]'),
+      tech3 = $('[data-no="tech-3"]');
+
+    // --- function to show data on click
+    const onclickTech = (planet, array) => {
+      planet.addEventListener("click", () => {
+        // call code block in loadTech function with array param
+        return loadTech(array);
+      });
+    };
+
+    // --- create content of all numbers
+    // --- numbers below are coming from techDefault(array)
+    onclickTech(tech1, 0);
+    onclickTech(tech2, 1);
+    onclickTech(tech3, 2);
   });
 
-// tabs test
-const tabMoon = document.querySelector("#moon");
-const tabMars = document.querySelector("#mars");
-const tabEurpa = document.querySelector("#europa");
-const tabTitan = document.querySelector("#titan");
+// TOGGLE MENU
+const toggleMenu = $(".toggle--menu");
+const closeMenu = $(".toggle--close");
 
-let changeTitle = response.destinations[1].name;
-tabMars.addEventListener("click", () => {
-  document.querySelector(".tabs__title").innerHTML = changeTitle;
+toggleMenu.addEventListener("click", () => {
+  nav.style.display = "flex";
+  closeMenu.style.display = "inline-block";
+  toggleMenu.style.display = "none";
+});
+
+closeMenu.addEventListener("click", () => {
+  nav.style.display = "none";
+  closeMenu.style.display = "none";
+  toggleMenu.style.display = "none";
 });
